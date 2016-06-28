c86428f9b73a49516bfae128c798a81ab1cfab1c
Augustyn <marcin.augustyn@capgemini.com>
2016-06-28 10:49:17 +0200

    added Portfolio

--- StockMarketWithApi/StockMarketApi/src/main/java/augustyn/marcin/stockmarket/rest/SimRestService.java
+++ StockMarketWithApi/StockMarketApi/src/main/java/augustyn/marcin/stockmarket/rest/SimRestService.java
@@ -2,6 +2,7 @@ package augustyn.marcin.stockmarket.rest;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,15 +12,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import augustyn.marcin.stockmarket.broker.to.OfferTo;
 import augustyn.marcin.stockmarket.main.Main;
-import augustyn.marcin.stockmarket.main.MainImpl;
 
 @Controller
 @ResponseBody
 @RequestMapping("/")
 public class SimRestService {
 	
-	//@Autowired
-	private Main main = new MainImpl();
+	@Autowired
+	private Main main;
 	
 	@RequestMapping(value = "/sim", method = RequestMethod.GET)
     public ResponseEntity<List<OfferTo>> simulate() {
