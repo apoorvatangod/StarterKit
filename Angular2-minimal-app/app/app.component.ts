a442ad05f95dad1b32ab32f9ff93e823994ce617
marciaug <marcin.augustyn@capgemini.com>
2016-12-22 09:03:13 +0100

    added angular2 minimal app

---
+++ Angular2-minimal-app/app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',//selector used in HTML
+  template: '<h1>Hello world!</h1>'//template/view of HTML page to be shown
+})
+export class AppComponent { }
